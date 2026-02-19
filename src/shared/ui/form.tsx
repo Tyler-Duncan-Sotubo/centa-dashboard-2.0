3a8e0b09a4fa7d508201f144fd4fd7138d6c3035
WeplugMusic <admin@weplugmusic.com>
2026-02-19 09:41:08 +0000

    update avatars

--- src/shared/ui/form.tsx
+++ src/shared/ui/form.tsx
@@ -166,7 +166,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-[0.8rem] font-medium text-destructive", className)}
+      className={cn("text-md font-medium text-destructive", className)}
       {...props}
     >
       {body}
